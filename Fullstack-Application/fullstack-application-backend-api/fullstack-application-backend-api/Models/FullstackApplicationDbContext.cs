26da8cae21643db732c658fd4353cacc00065b5c
Raphael <raphavtei@gmail.com>
2026-01-06 17:27:16 -0300

    Fullstack App Checkpoint

---
+++ Fullstack-Application/fullstack-application-backend-api/fullstack-application-backend-api/Models/FullstackApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace fullstack_application_backend_api.Models;
+
+public partial class FullstackApplicationDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("fullstack_application");
+    }
+    public FullstackApplicationDbContext()
+    {
+    }
+
+    public FullstackApplicationDbContext(DbContextOptions<FullstackApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Propertylisting> Propertylistings { get; set; }
+}
+
+ 
---
+++ Fullstack-Application/fullstack-application-backend-api/fullstack-application-backend-api/Models/Propertylisting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fullstack_application_backend_api.Models;
+
+[Table("propertylistings")]
+public partial class Propertylisting
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("property_type")]
+    public string PropertyType { get; set; } = null!;
+    [Column("property_value")]
+    public decimal PropertyValue { get; set; }
+    [Column("property_info")]
+    public string PropertyInfo { get; set; } = null!;
+}
