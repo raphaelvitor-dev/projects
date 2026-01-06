b06022f09f4b5fb8f45603976e41d8e5958237c9
Raphael <raphavtei@gmail.com>
2026-01-06 17:35:19 -0300

    Project Update

--- gestaltsite-main/js/typedanimation.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed('#typetext', {
-    strings: ["Excellence.", "Solutions.", "Brilliance.", "Innovation.", "Mastery.", "Impact.", "Perfection.", "Magic.", "Power.", "Creativity."],
-    typeSpeed: 50,
-    fadeOut: true,
-    fadeOutClass: 'typed-fade-out',
-    startDelay: 1000,
-    loop:true
-  });
